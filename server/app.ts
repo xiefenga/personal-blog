4ef4d26b0fb05c61fbd59a33ba579e4dd29d54fe
xf <1294699027@qq.com>
2021-05-06 22:46:33 +0800

    add server

---
+++ server/app.ts
@@ -0,0 +1,17 @@
+import Koa from 'koa'
+import path from 'path'
+import cors from '@koa/cors'
+import koaStatic from 'koa-static'
+import bodyParser from 'koa-bodyparser'
+import 'reflect-metadata'
+import './db/connect'
+
+const app = new Koa();
+
+app.use(cors());
+
+app.use(koaStatic(path.resolve(__dirname, './public')));
+
+app.use(bodyParser());
+
+app.listen(8080, () => console.log('server is running on 8080'));
