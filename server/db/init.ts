bfab7aea47f2094f6f3592560443f736191af1a8
xf <1294699027@qq.com>
2021-05-11 22:23:02 +0800

    update db connection

--- server/db/init.ts
+++ server/db/init.ts
@@ -1,5 +1,5 @@
-import sequelize from './db'
+import sequelize from './connection'
 
-sequelize.sync({ force: true })
+sequelize.sync()
   .then(() => console.log('所有模型同步完成'))
   .catch(err => console.log('模型同步失败', '\n', err));
