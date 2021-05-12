f96e57fa15baa2e2c31891e1d960e0ad91a5ed1b
xf <1294699027@qq.com>
2021-05-12 23:45:27 +0800

    change categories check

--- server/validation/article.ts
+++ server/validation/article.ts
@@ -1,21 +1,25 @@
 import TagEntity from '../db/entities/Tag'
 import CategoryEntity from '../db/entities/Category'
 
-async function categoriesCheck(categories: number[][]): Promise<boolean> {
-  for (const [c1, c2] of categories) {
-    if (!c2) {
-      const c = await CategoryEntity.findByPk(c1);
-      if (!c) {
-        return false;
-      }
-    } else {
-      const c = await CategoryEntity.findByPk(c2);
-      if (!c || c.parentId === c1) {
-        return false;
-      }
-    }
-  }
-  return true;
+// async function categoriesCheck(categories: number[][]): Promise<boolean> {
+//   for (const [c1, c2] of categories) {
+//     if (!c2) {
+//       const c = await CategoryEntity.findByPk(c1);
+//       if (!c) {
+//         return false;
+//       }
+//     } else {
+//       const c = await CategoryEntity.findByPk(c2);
+//       if (!c || c.parentId === c1) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+async function categoriesCheck(categories: number[]): Promise<boolean> {
+  const res = await Promise.all(categories.map(id => CategoryEntity.findByPk(id)));
+  return !res.includes(null);
 }
 
 async function tagsCheck(tags: number[]): Promise<boolean> {
