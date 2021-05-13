fa79395dd0665fa5e00480cebd191463897d723b
xf <1294699027@qq.com>
2021-05-13 23:24:20 +0800

    complete category route

--- server/routes/category.ts
+++ server/routes/category.ts
@@ -1,37 +1,35 @@
 import Router from '@koa/router'
-import { addCategory, getCategories } from '../services/category'
+import { createFailResponse, createSuccessResponse } from '../utils/create'
+import { addCategory, deleteCategory, getCategories, updateCategory } from '../services/category'
 
 const router = new Router();
 
 router.get('/', async ctx => {
-  ctx.body = await getCategories();
-});
-
-router.get('/:id', async ctx => {
-  const { id } = ctx.params;
-  const { page, size } = ctx.request.query;
-  ctx.body = ctx.url;
-});
-
-router.get('/:pid/:id', async ctx => {
-  const { pid, id } = ctx.params;
-  const { page, size } = ctx.request.query;
-  ctx.body = ctx.url;
+  const [categories, count] = await getCategories();
+  ctx.body = createSuccessResponse(categories, count);
 });
 
 router.post('/', async ctx => {
   const res = await addCategory(ctx.request.body);
-  ctx.body = res;
+  ctx.body = Array.isArray(res)
+    ? createFailResponse(res)
+    : createSuccessResponse(res);
 });
 
 router.put('/:id', async ctx => {
   const { id } = ctx.params;
-  ctx.body = ctx.url;
+  const res = await updateCategory(Number(id), ctx.request.body);
+  ctx.body = Array.isArray(res)
+    ? createFailResponse(res)
+    : createSuccessResponse();
 });
 
 router.delete('/:id', async ctx => {
   const { id } = ctx.params;
-  ctx.body = ctx.url;
+  const res = await deleteCategory(Number(id));
+  ctx.body = Array.isArray(res)
+    ? createFailResponse(res)
+    : createSuccessResponse();
 });
 
-export default router;
+export default router
