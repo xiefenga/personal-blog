2bc86f126295537bb2c042c042f8843ea64c2c90
xf <1294699027@qq.com>
2021-05-13 23:24:30 +0800

    complete tag route

--- server/routes/tag.ts
+++ server/routes/tag.ts
@@ -1,17 +1,35 @@
 import Router from '@koa/router'
-import { addTag } from '../services/tag'
+import { addTag, deleteTag, getTags, updateTag } from '../services/tag'
+import { createFailResponse, createSuccessResponse } from '../utils/create'
 
 const router = new Router();
 
 router.get('/', async ctx => {
-
+  const [data, count] = await getTags();
+  ctx.body = createSuccessResponse(data, count);
 });
 
 router.post('/', async ctx => {
   const res = await addTag(ctx.request.body);
   ctx.body = Array.isArray(res)
-    ? { status: 'fail', error: res }
-    : { status: 'success', data: res };
+    ? createFailResponse(res)
+    : createSuccessResponse(res);
 });
+
+router.put('/:id', async ctx => {
+  const { id } = ctx.params;
+  const res = await updateTag(Number(id), ctx.request.body);
+  ctx.body = Array.isArray(res)
+    ? createFailResponse(res)
+    : createSuccessResponse(res);
+});
+
+router.delete('/:id', async ctx => {
+  const { id } = ctx.params;
+  const res = await deleteTag(Number(id));
+  ctx.body = Array.isArray(res)
+    ? createFailResponse(res)
+    : createSuccessResponse();
+})
 
 export default router
