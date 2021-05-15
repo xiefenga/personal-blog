72ff946f9bc19eeff683e798b83996e73c58646f
xf <1294699027@qq.com>
2021-05-15 20:49:55 +0800

    add utils

---
+++ admin/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const THROTTLE_MD_RENDER_TIME = 100;
+
+export const editorOptions = {
+  theme: 'md-mirror',
+  keyMap: 'sublime',
+  mode: 'markdown',
+  lineWrapping: true,
+  lineNumbers: false,
+}
---
+++ admin/src/utils/markdown.js
@@ -0,0 +1,27 @@
+import MarkdownIt from 'markdown-it'
+import highlightjs from 'highlight.js'
+
+const markdownParser = new MarkdownIt({
+  html: true,
+  highlight: (str, lang) => {
+    if (lang === undefined || lang === "") {
+      lang = "bash";
+    }
+    // 加上custom则表示自定义样式，而非微信专属，避免被remove pre
+    if (lang && highlightjs.getLanguage(lang)) {
+      try {
+        const formatted = highlightjs
+          .highlight(lang, str, true)
+          .value.replace(/\n/g, "<br/>") // 换行用br表示
+          .replace(/\s/g, "&nbsp;") // 用nbsp替换空格
+          .replace(/span&nbsp;/g, "span "); // span标签修复
+        return '<pre class="custom"><code class="hljs">' + formatted + "</code></pre>";
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    return '<pre class="custom"><code class="hljs">' + markdownParser.utils.escapeHtml(str) + "</code></pre>";
+  },
+});
+
+export { markdownParser }
