d15113e4a44dea9a0d51ecb511f2c56326f2452b
xf <1294699027@qq.com>
2021-05-17 21:00:23 +0800

    add admin route

--- server/middlewares/router.ts
+++ server/middlewares/router.ts
@@ -1,10 +1,15 @@
 import Router from '@koa/router'
 import tagRouter from '../routes/tag'
+import adminRouter from '../routes/admin'
 import articleRouter from '../routes/article'
 import categoryRouter from '../routes/category'
 import archivesRouter from '../routes/archives'
 
 const router = new Router({ prefix: '/api' });
+
+router.use('/tag', tagRouter.routes());
+
+router.use('/admin', adminRouter.routes());
 
 router.use('/article', articleRouter.routes());
 
@@ -12,6 +17,5 @@ router.use('/category', categoryRouter.routes());
 
 router.use('/archives', archivesRouter.routes());
 
-router.use('/tag', tagRouter.routes());
 
 export default router
