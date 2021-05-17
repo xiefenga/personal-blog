775b09587db599e2ce129b231e28ada93e77faf8
xf <1294699027@qq.com>
2021-05-17 20:58:48 +0800

    add createFailResponse

--- server/middlewares/error.ts
+++ server/middlewares/error.ts
@@ -1,14 +1,14 @@
+import { createFailResponse } from '../utils/response'
 import { ParameterizedContext, DefaultState, DefaultContext, Next } from 'koa'
 
-const errorMiddleware = () => async (ctx: ParameterizedContext<DefaultState, DefaultContext>, next: Next) => {
-  try {
-    await next();
-  } catch (error) {
-    ctx.body = {
-      status: 'fail',
-      error: error.message
+const errorMiddleware = () => (
+  async (ctx: ParameterizedContext<DefaultState, DefaultContext>, next: Next) => {
+    try {
+      await next();
+    } catch (error) {
+      ctx.body = createFailResponse(error.message);
     }
   }
-}
+);
 
 export default errorMiddleware
