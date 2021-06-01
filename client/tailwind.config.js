750fadebe4bb6779ec204725cec05b46d38fb2a9
xf <1294699027@qq.com>
2021-06-01 15:24:12 +0800

    add tailwindcss

---
+++ client/tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// <!-- Dark mode not enabled -->
+// <html>
+// <body>
+//   <!-- Will be white -->
+//   <div class="bg-white dark:bg-black">
+//     <!-- ... -->
+//   </div>
+// </body>
+// </html>
+
+// <!-- Dark mode enabled -->
+// <html class="dark">
+// <body>
+//   <!-- Will be black -->
+//   <div class="bg-white dark:bg-black">
+//     <!-- ... -->
+//   </div>
+// </body>
+// </html>
