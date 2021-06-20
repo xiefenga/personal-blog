6d1664caad165ff0eb221c8518b35cefeb8c41e9
xf <1294699027@qq.com>
2021-06-20 09:44:10 +0800

    新增加载状态

--- admin/src/components/ArticleEditor/index.jsx
+++ admin/src/components/ArticleEditor/index.jsx
@@ -1,24 +1,46 @@
+import { Spin } from 'antd'
 import Toolbar from './Toolbar'
-import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useGetArticle } from '@/hooks/store'
 import MarkdownEditor from './MarkdownEditor'
+import { LoadingOutlined } from '@ant-design/icons'
+import { useEffect, useCallback, useState } from 'react'
 import './index.css'
+
 
 
 function ArticleEditor() {
   const { id } = useParams();
   const getArticle = useGetArticle();
 
-  useEffect(
-    () => id && getArticle(id),
+  const [loading, setLoading] = useState(false);
+
+  const init = useCallback(
+    async () => {
+      if (id) {
+        setLoading(true);
+        await getArticle(id);
+        setLoading(false);
+      }
+    },
     [id, getArticle]
-  );
+  )
+
+  useEffect(init, [init]);
 
   return (
     <div className="article-editor">
-      <Toolbar />
-      <MarkdownEditor />
+      <Spin
+        wrapperClassName="editor-container"
+        size="large"
+        delay={100}
+        tip="获取文章内容"
+        spinning={loading}
+        indicator={<LoadingOutlined spin />}
+      >
+        <Toolbar />
+        <MarkdownEditor />
+      </Spin>
     </div>
   )
 }
